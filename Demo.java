18b6b3947773cab3d85554de5eb67c4fc0c79b1d
AHETESHAMS <aheteshams007@gmail.com>
2024-02-27 09:33:32 +0530

    Added UC 2

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ class Demo{
 
    public static void main(String []args){
 	System.out.println("Welcome");
-	System.out.println("UC 1")
+	System.out.println("UC 1");
+	System.out.pritnln("UC 2");
    }
 }
